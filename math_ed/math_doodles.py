90962cf6b61b2c27946f43d187c0c74e6a540243
Duncan Murchison <duncan@computeranything.dev>
2024-02-06 15:35:29 -0500

    :rocket: First Commit

---
+++ math_ed/math_doodles.py
@@ -0,0 +1,22 @@
+import math
+
+# print the area and the perimeter and the diagonal of a rectangle
+
+l = 5
+w = 3
+
+
+def rectangle_area(l, w):
+    return l * w
+
+def rectangle_perimeter(l, w):
+    return 2 * (l + w)
+
+def rectangle_diagonal(l, w):
+    return math.sqrt(l**2 + w**2)
+
+print(f"The area of a rectangle with length {l} and width {w} is {rectangle_area(l, w)}")
+print(f"The perimeter of a rectangle with length {l} and width {w} is {rectangle_perimeter(l, w)}")
+print(f"The diagonal of a rectangle with length {l} and width {w} is {rectangle_diagonal(l, w)}")
+
+
