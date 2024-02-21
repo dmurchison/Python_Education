a89e6788fc54de57b90cbef155da7b35a34625e9
Duncan Murchison <duncan@computeranything.dev>
2024-02-21 13:43:58 -0500

    :ambulance: Removed unneeded files

--- math_ed/math_dsa.py
+++ math_ed/math_dsa.py
@@ -4,7 +4,7 @@ n = 10
 def tester(n):
     for i in range(n + 1):
         if i%3==0 and i%5==0:
-            print(f"{i} is divisible by 3 and 5")
+            print("FizzBuzz")
         elif i%3==0:
             print("Fizz")
         elif i%5==0:
