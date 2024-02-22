e3441ccbcd083ef81e4c1e3f0652e19f12c16a67
Duncan Murchison <duncan@computeranything.dev>
2024-02-22 12:33:45 -0500

    ♻️ Refactor FizzBuzz logic and add single-threaded CPU-bound function

--- math_ed/math_dsa.py
+++ math_ed/math_dsa.py
@@ -3,11 +3,19 @@ n = 10
 
 def tester(n):
     for i in range(n + 1):
-        if i%3==0 and i%5==0:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
-        elif i%3==0:
+        elif i % 3 == 0:
             print("Fizz")
-        elif i%5==0:
+        elif i % 5 == 0:
             print("Buzz")
+        else:
+            print(f"The number {i} is not divisible by 3 or 5.")
 
 tester(n)
+
+def single_thread_cpu_bound(n):
+    for i in range(n):
+        print(i**2)
+
+single_thread_cpu_bound(n)
